903472fcf52e61abe111057dfefa6606c2e72c20
maxild <mmaxild@gmail.com>
2016-08-31 18:43:05 +0200

    Adding cake and pushing packages to maxfire-ci myget feed

--- build.ps1
+++ build.ps1
@@ -1,113 +1,181 @@
-# The cmdlet binding makes common parameters available. The common parameters are
-# -Verbose
-# -Debug
-# -WarningAction
-# -WarningVariable
-# -ErrorAction
-# -ErrorVariable
-# -OutVariable
-# -OutBuffer
+##########################################################################
+# This is the Cake bootstrapper script for PowerShell.
+# This file was downloaded from https://github.com/cake-build/resources
+# Feel free to change this file to fit your needs.
+##########################################################################
+
+<#
+
+.SYNOPSIS
+This is a Powershell script to bootstrap a Cake build.
+
+.DESCRIPTION
+This Powershell script will download NuGet if missing, restore NuGet tools (including Cake)
+and execute your Cake build script with the parameters you provide.
+
+.PARAMETER Script
+The build script to execute.
+.PARAMETER Target
+The build script target to run.
+.PARAMETER Configuration
+The build configuration to use.
+.PARAMETER Verbosity
+Specifies the amount of information to be displayed.
+.PARAMETER ShowVersion
+Show version of Cake tool.
+.PARAMETER Experimental
+Tells Cake to use the latest Roslyn release.
+.PARAMETER WhatIf
+Performs a dry run of the build script.
+No tasks will be executed.
+.PARAMETER Mono
+Tells Cake to use the Mono scripting engine.
+.PARAMETER SkipToolPackageRestore
+Skips restoring of packages.
+.PARAMETER ScriptArgs
+Remaining arguments are added here.
+
+.LINK
+http://cakebuild.net
+
+#>
+
 [CmdletBinding()]
-param (
-    [ValidateSet("debug", "release")]
-    [Alias('config')]
-    [string]$Configuration = 'debug',
-    [ValidateSet("Release", `
-                 "alpha", `
-                 "beta1", "beta2", "beta3", "beta4", "beta5", `
-                 "rc1", "rc2", "rc3", "rc4", "rc5", `
-                 "local")]
-    [Alias('label')]
-    [string]$PrereleaseTag = 'local',
-    [ValidateRange(1,99999)]
-    [Alias('build')]
-    [int]$BuildNumber,
-    [string]$CommitId = "0000000000000000000000000000000000000000",
-    [switch]$SkipRestore,
-    [switch]$CleanCache,
-    [switch]$SkipTests
+Param(
+    [string]$Script = "build.cake",
+    [string]$Target = "Default",
+    [string]$Configuration = "Release",
+    [ValidateSet("Quiet", "Minimal", "Normal", "Verbose", "Diagnostic")]
+    [string]$Verbosity = "Verbose",
+    [switch]$ShowVersion,
+    [switch]$Experimental,
+    [Alias("DryRun","Noop")]
+    [switch]$WhatIf,
+    [switch]$Mono,
+    [switch]$SkipToolPackageRestore,
+    [Parameter(Position=0,Mandatory=$false,ValueFromRemainingArguments=$true)]
+    [string[]]$ScriptArgs
 )
 
-trap
+[Reflection.Assembly]::LoadWithPartialName("System.Security") | Out-Null
+function MD5HashFile([string] $filePath)
 {
-    Write-Host "Build failed: $_" -ForegroundColor Red
-    Write-Host $_.Exception -ForegroundColor Red
-    Write-Host ("`r`n" * 3)
-    exit 1
+    if ([string]::IsNullOrEmpty($filePath) -or !(Test-Path $filePath -PathType Leaf))
+    {
+        return $null
+    }
+
+    [System.IO.Stream] $file = $null;
+    [System.Security.Cryptography.MD5] $md5 = $null;
+    try
+    {
+        $md5 = [System.Security.Cryptography.MD5]::Create()
+        $file = [System.IO.File]::OpenRead($filePath)
+        return [System.BitConverter]::ToString($md5.ComputeHash($file))
+    }
+    finally
+    {
+        if ($file -ne $null)
+        {
+            $file.Dispose()
+        }
+    }
 }
 
-. "$PSScriptRoot\build\common.ps1"
+$PSScriptRoot = split-path -parent $MyInvocation.MyCommand.Definition;
 
-# TODO: Use Psake
-# Clean-Projects
-#return
+# Tree
+$BUILD_DIR           = Join-Path $PSScriptRoot "build" # build scripts, maybe rename
+$TOOLS_DIR           = Join-Path $PSScriptRoot ".tools"
+$NUGET_EXE           = Join-Path $TOOLS_DIR "nuget.exe"
+$CAKE_EXE            = Join-Path $TOOLS_DIR "Cake/Cake.exe"
+$PACKAGES_CONFIG     = Join-Path $BUILD_DIR "packages.config" # containing Cake dependency
+$PACKAGES_CONFIG_MD5 = Join-Path $TOOLS_DIR "packages.config.md5sum"
 
-# Write prologue
-Write-Host ("`r`n" * 3)
-Say ('=' * 60)
+# Aliases for the entire powershell session
+Set-Alias nuget $NUGET_EXE -scope global
+Set-Alias cake  $CAKE_EXE -scope global
 
-$startTime = [DateTime]::UtcNow
-if (-not $BuildNumber) {
-    $BuildNumber = Get-BuildNumber
-}
-Say "Build #$BuildNumber started at $startTime"
-
-Push-Location $RepoRoot
-
-$BuildErrors = @()
-Invoke-BuildStep 'Cleaning artifacts' { Clear-Artifacts } `
-    -ev +BuildErrors
-
-Invoke-BuildStep 'Cleaning package cache' { Clear-PackageCache } `
-    -skip:(-not $CleanCache) `
-    -ev +BuildErrors
-
-Invoke-BuildStep 'Installing NuGet.exe' { Install-NuGet -Prerelease } `
-    -ev +BuildErrors
-
-Invoke-BuildStep 'Installing dotnet CLI' { Install-DotnetCLI } `
-    -ev +BuildErrors
-
-Invoke-BuildStep 'Restoring solution packages' { Restore-SolutionPackages } `
-    -skip:$SkipRestore `
-    -ev +BuildErrors
-
-Invoke-BuildStep 'Building packages' {
-        param($Configuration, $BuildLabel, $BuildNumber, $SkipRestore, $Fast)
-        # This will build and package all production code under ./src folder
-        BuildPackages $Configuration $BuildLabel $BuildNumber -SkipRestore:$SkipRestore
-    } `
-    -args $Configuration, $PrereleaseTag, $BuildNumber, $SkipRestore `
-    -ev +BuildErrors
-
-Invoke-BuildStep 'Running tests' {
-        param($SkipRestore)
-        # This will build and execute all tests under the ./test folder
-        RunTests -SkipRestore:$SkipRestore
-    } `
-    -args $SkipRestore `
-    -skip:$SkipTests `
-    -ev +BuildErrors
-
-Pop-Location
-
-# Write epilogue
-Say ('-' * 60)
-
-# TODO: Use stopwatch
-$endTime = [DateTime]::UtcNow
-Say "Build #$BuildNumber ended at $endTime"
-Say "Time elapsed $(Format-ElapsedTime ($endTime - $startTime))"
-
-if ($BuildErrors) {
-    Say "Build's completed with following errors:"
-    $BuildErrors | Out-Default
+# Should we use mono?
+$UseMono = "";
+if($Mono.IsPresent) {
+    Write-Verbose -Message "Using the Mono based scripting engine."
+    $UseMono = "-mono"
 }
 
-Say ('=' * 60)
-
-if ($BuildErrors) {
-    Throw $BuildErrors.Count
+# Should we use the new Roslyn?
+$UseExperimental = "";
+if($Experimental.IsPresent -and (-not ($Mono.IsPresent))) {
+    Write-Verbose -Message "Using experimental version of Roslyn."
+    $UseExperimental = "-experimental"
 }
 
-Write-Host ("`r`n" * 3)
+# Is this a dry run?
+$UseDryRun = "";
+if($WhatIf.IsPresent) {
+    Write-Verbose -Message "Performs a dry run of the build script."
+    $UseDryRun = "-dryrun"
+}
+
+# Make sure .tools folder exists
+if ((Test-Path $PSScriptRoot) -and (-not (Test-Path $TOOLS_DIR))) {
+    Write-Verbose -Message "Creating tools directory..."
+    New-Item -Path $TOOLS_DIR -Type directory | out-null
+}
+
+# Download NuGet if it does not exist.
+if (-not (Test-Path $NUGET_EXE)) {
+    Write-Verbose -Message "Downloading NuGet.exe..."
+    try {
+        Invoke-WebRequest "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" -OutFile $NUGET_EXE
+    } catch {
+        Throw "Could not download NuGet.exe."
+    }
+}
+
+# Install/restore tools (i.e. Cake) using NuGet
+if(-not $SkipToolPackageRestore.IsPresent) {
+    Push-Location
+    Set-Location $TOOLS_DIR
+
+    # Check for changes in packages.config and remove installed tools if true.
+    [string] $md5Hash = MD5HashFile $PACKAGES_CONFIG
+    if ( (-not (Test-Path $PACKAGES_CONFIG_MD5)) -Or
+      ($md5Hash -ne (Get-Content $PACKAGES_CONFIG_MD5 )) ) {
+        Write-Verbose -Message "Missing or changed packages.config hash..."
+        Remove-Item * -Recurse -Exclude nuget.exe # remove installed tools (ie. cake)
+    }
+
+    Write-Verbose -Message "Restoring tools from NuGet..."
+    $NuGetOutput = &nuget install $PACKAGES_CONFIG -ExcludeVersion -OutputDirectory `"$TOOLS_DIR`"
+    if ($LASTEXITCODE -ne 0) {
+        Throw "An error occured while restoring NuGet tools."
+    }
+    else
+    {
+        # save packages.config hash to disk
+        $md5Hash | Out-File $PACKAGES_CONFIG_MD5 -Encoding "ASCII"
+    }
+
+    Write-Verbose -Message ($NuGetOutput | out-string)
+    Pop-Location
+}
+
+# Make sure that Cake has been installed.
+if (-not (Test-Path $CAKE_EXE)) {
+    Throw "Could not find Cake.exe at $CAKE_EXE"
+}
+
+# Start Cake
+if($ShowVersion.IsPresent) {
+    &cake -version
+}
+else {
+    Write-Host "Running build script..."
+    # C# v6 features (e.g. string interpolation) are not supported without '-experimental' flag
+    #   See https://github.com/cake-build/cake/issues/293
+    #   See https://github.com/cake-build/cake/issues/326
+    #&cake $Script -experimental -target="$Target" -configuration="$Configuration" -verbosity="$Verbosity" $UseMono $UseDryRun $UseExperimental $ScriptArgs
+    &cake $Script -target="$Target" -configuration="$Configuration" -verbosity="$Verbosity" $UseMono $UseDryRun $UseExperimental $ScriptArgs
+}
+exit $LASTEXITCODE
--- build.sh
+++ build.sh
@@ -1,136 +1,101 @@
 #!/usr/bin/env bash
 
-while true ; do
-	case "$1" in
-		-c|--clear-cache) CLEAR_CACHE=1 ; shift ;;
-		--) shift ; break ;;
-		*) shift ; break ;;
-	esac
+##########################################################################
+# This is the Cake bootstrapper script for Linux and OS X.
+# This file was downloaded from https://github.com/cake-build/resources
+# Feel free to change this file to fit your needs.
+##########################################################################
+
+# Define directories.
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+BUILD_DIR=$SCRIPT_DIR/build # build scripts, maybe rename
+TOOLS_DIR=$SCRIPT_DIR/.tools
+NUGET_EXE=$TOOLS_DIR/nuget.exe
+CAKE_EXE=$TOOLS_DIR/Cake/Cake.exe
+PACKAGES_CONFIG=$BUILD_DIR/packages.config
+PACKAGES_CONFIG_MD5=$TOOLS_DIR/packages.config.md5sum
+
+# Define md5sum or md5 depending on Linux/OSX
+MD5_EXE=
+if [[ "$(uname -s)" == "Darwin" ]]; then
+    MD5_EXE="md5 -r"
+else
+    MD5_EXE="md5sum"
+fi
+
+# Define default arguments.
+SCRIPT="build.cake"
+TARGET="Default"
+CONFIGURATION="Release"
+VERBOSITY="verbose"
+DRYRUN=
+SHOW_VERSION=false
+SCRIPT_ARGUMENTS=()
+
+# Parse arguments.
+for i in "$@"; do
+    case $1 in
+        -s|--script) SCRIPT="$2"; shift ;;
+        -t|--target) TARGET="$2"; shift ;;
+        -c|--configuration) CONFIGURATION="$2"; shift ;;
+        -v|--verbosity) VERBOSITY="$2"; shift ;;
+        -d|--dryrun) DRYRUN="-dryrun" ;;
+        --version) SHOW_VERSION=true ;;
+        --) shift; SCRIPT_ARGUMENTS+=("$@"); break ;;
+        *) SCRIPT_ARGUMENTS+=("$1") ;;
+    esac
+    shift
 done
 
-RESULTCODE=0
-
-configuration="release"
-
-rootFolder=$(dirname "${BASH_SOURCE}")
-artifactsFolder="./artifacts"
-
-# clear artifacts
-if [[ -d $artifactsFolder ]]; then  
-  rm -R $artifactsFolder
+# Make sure the tools folder exist.
+if [ ! -d "$TOOLS_DIR" ]; then
+  mkdir "$TOOLS_DIR"
 fi
 
-# One liner...
-# curl -sSL https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0-preview1/scripts/obtain/dotnet-install.sh | bash /dev/stdin --version 1.0.0-preview1-002702 --install-dir ~/dotnet
-
-# Download the CLI install script
-echo "Installing dotnet CLI"
-mkdir -p .dotnetcli
-curl -o .dotnetcli/dotnet-install.sh https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0-preview2/scripts/obtain/dotnet-install.sh
-
-# Define the install root for the script (it defaults to ~/.dotnet)
-export DOTNET_INSTALL_DIR="$PWD/.dotnetcli"
-
-# Define the DOTNET_HOME directory for tools in the CLI
-export DOTNET_HOME=$DOTNET_INSTALL_DIR
-
-# Run dotnet-install.sh
-chmod +x .dotnetcli/dotnet-install.sh
-.dotnetcli/dotnet-install.sh --install-dir .dotnetcli -channel preview --version 1.0.0-preview2-003121 --no-path
-
-# Display current version
-DOTNET="$DOTNET_INSTALL_DIR/dotnet"
-$DOTNET --version
-
-# clear caches
-if [ "$CLEAR_CACHE" == "1" ]
-then
-	# echo "Clearing the nuget web cache folder"
-	# rm -r -f ~/.local/share/NuGet/*
-
-	echo "Clearing the nuget packages folder"
-	rm -r -f ~/.nuget/packages/*
+# Download NuGet if it does not exist.
+if [ ! -f "$NUGET_EXE" ]; then
+    echo "Downloading NuGet..."
+    curl -Lsfo "$NUGET_EXE" https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+    if [ $? -ne 0 ]; then
+        echo "An error occured while downloading nuget.exe."
+        exit 1
+    fi
 fi
 
-# restore packages
-echo "dotnet restore src test --verbosity minimal"
-$DOTNET restore src test --verbosity minimal
+# Install/restore tools (i.e. Cake) using NuGet
+pushd "$TOOLS_DIR" >/dev/null
 
+# Check for changes in packages.config and remove installed tools if true.
+    if [ ! -f $PACKAGES_CONFIG_MD5 ] || [ "$( cat $PACKAGES_CONFIG_MD5 | sed 's/\r$//' )" != "$( $MD5_EXE $PACKAGES_CONFIG | awk '{ print $1 }' )" ]; then
+    find . -type d ! -name . | xargs rm -rf
+fi
+
+mono "$NUGET_EXE" install $PACKAGES_CONFIG -ExcludeVersion
 if [ $? -ne 0 ]; then
-	echo "Restore failed!!"
-	exit 1
+    echo "Could not restore NuGet packages."
+    exit 1
 fi
 
-# run tests
-for testProject in `find test -type f -name project.json`
-do
-    # Get the test project name 
-	testDir="$(pwd)/$(dirname $testProject)"
-    testProjectName="$(basename $testDir)"
+# save packages.config hash to disk
+$MD5_EXE $PACKAGES_CONFIG | awk '{ print $1 }' >| $PACKAGES_CONFIG_MD5
 
-    echo "Running tests for $testProjectName"
+popd >/dev/null
 
-	pushd $testDir
-
-    # Ideally we would use the 'dotnet test' command to test both netcoreapp1.0 and net46,
-    # but this currently doesn't work due to https://github.com/dotnet/cli/issues/3073
-	echo "dotnet test $testDir --configuration $configuration --framework netcoreapp1.0"
-	$DOTNET test $testDir --configuration $configuration --framework netcoreapp1.0
-
-    if [ $? -ne 0 ]; then
-        echo "$testProjectName failed on .NET Core!!"
-        RESULTCODE=1
-    fi
-
-    # Instead, build with .NET Core SDK (dotnet build)... 
-    echo "dotnet build $testDir --configuration $configuration --framework net46"
-	$DOTNET build $testDir --configuration $configuration --framework net46
-
-    if [ $? -ne 0 ]; then
-        echo "$testProjectName failed to build for net46!!"
-        RESULTCODE=1
-    fi
-
-    # ..., and run xUnit.net .NET CLI test runner directly with mono for the full/desktop .net version
-    echo "mono ./bin/$configuration/net46/*/dotnet-test-xunit.exe ./bin/$configuration/net46/*/$testProjectName.dll"
-    mono $testDir/bin/$configuration/net46/*/dotnet-test-xunit.exe $testDir/bin/$configuration/net46/*/$testProjectName.dll
-
-    if [ $? -ne 0 ]; then
-        echo "$testProjectName failed on Mono!!"
-        RESULTCODE=1
-    fi
-
-	popd
-
-done
-
-if [ $RESULTCODE -ne 0 ]; then
-	echo "Tests failed!!"
-	exit $RESULTCODE 
+# Make sure that Cake has been installed.
+if [ ! -f "$CAKE_EXE" ]; then
+    echo "Could not find Cake.exe at '$CAKE_EXE'."
+    exit 1
 fi
 
-#TODO: revision from args (local-xxxx default)
-#revision=${TRAVIS_JOB_ID:=1}  
-#revision=$(printf "%04d" $revision)
-revision="local-12345"
-
-for srcProject in `find src -type f -name project.json`
-do
-    # Get the project name 
-	srcDir="$(pwd)/$(dirname $srcProject)"
-    srcProjectName="$(basename $srcDir)"
-
-    echo "Build package for $srcProjectName"
-    echo "================="
-    
-    echo "dotnet pack $srcDir -c $configuration -o $artifactsFolder --version-suffix=$revision"
-    $DOTNET pack $srcDir -c $configuration -o $artifactsFolder --version-suffix=$revision  
-
-    if [ $? -ne 0 ]; then
-        echo "$srcProjectName failed to pack!!"
-        RESULTCODE=1
-    fi
-
-done
-
-exit $RESULTCODE
+# Start Cake
+if $SHOW_VERSION; then
+    exec mono "$CAKE_EXE" -version
+else
+# C# v6 features (e.g. string interpolation) are not supported without '-experimental' flag
+#   See https://github.com/cake-build/cake/issues/293
+#   See https://github.com/cake-build/cake/issues/326
+# TODO: Is -experimental necessary on mono?
+    #exec mono "$CAKE_EXE" $SCRIPT -experimental -verbosity=$VERBOSITY -configuration=$CONFIGURATION -target=$TARGET $DRYRUN "${SCRIPT_ARGUMENTS[@]}"
+    exec mono "$CAKE_EXE" $SCRIPT -verbosity=$VERBOSITY -configuration=$CONFIGURATION -target=$TARGET $DRYRUN "${SCRIPT_ARGUMENTS[@]}"
+fi
