4f44f5fac01971c823c58963fe7db62454ce8dd3
maxild <mmaxild@gmail.com>
2018-08-21 14:49:16 +0200

    Map and Each enumerable extensions ported from Lofus

--- build.ps1
+++ build.ps1
@@ -54,7 +54,7 @@ Param(
     [string[]]$ScriptArgs
 )
 
-$PSScriptRoot = split-path -parent $MyInvocation.MyCommand.Definition;
+$PSScriptRoot = split-path -parent $MyInvocation.MyCommand.Definition
 
 # Tree
 $TOOLS_DIR           = Join-Path $PSScriptRoot "tools"
@@ -66,10 +66,16 @@ $PACKAGES_CONFIG_MD5 = Join-Path $TOOLS_DIR "packages.config.md5sum"
 # Maxfire.CakeScripts version can be pinned
 $CakeScriptsVersion = "latest" # 'latest' or 'major.minor.patch'
 
+# We are using Current releases
+#    not LTS - most current supported release
+#    not x.y version (1.1 or 2.0)
+# INVESTIGATE: Channel has no effect when -Version is specified. -Channel is used as a way
+#              fetch a value for -Version
+$DotNetChannel = "Current" # Current - most current release
 # .NET Core SDK version
-$DotNetSdkVersion = "2.1.4"
+$DotNetSdkVersion = "2.1.302" # 'latest' or x.y.z is supported
 # .NET Core Runtime version (older release/runtime to install)
-$DotNetRuntimeVersion="1.1.6" # TODO: How to specify latest 1.1.x release?
+$DotNetRuntimeVersions="1.1.9;2.0.9" # pinned runtimes, but we do not pin 2.1.x runtime????? maybe use channel 1.1 and 2.0???
 
 if ((-not ($NuGetVersion -eq "latest")) -and (-not $NuGetVersion.StartsWith("v"))) {
     $NuGetVersion = ("v" + $NuGetVersion)
@@ -83,7 +89,7 @@ if ((Test-Path $PSScriptRoot) -and (-not (Test-Path $TOOLS_DIR))) {
 }
 
 ###########################################################################
-# Install .NET Core CLI
+# Install .NET Core SDK
 ###########################################################################
 
 Function Remove-PathVariable([string]$VariableToRemove)
@@ -103,34 +109,71 @@ Function Remove-PathVariable([string]$VariableToRemove)
     }
 }
 
-# Get .NET Core CLI path if installed.
-$FoundDotNetCliVersion = $null;
+$FoundDotNetSdkVersion = $null
 if (Get-Command dotnet -ErrorAction SilentlyContinue) {
-    $FoundDotNetCliVersion = dotnet --version;
+    $FoundDotNetSdkVersion = dotnet --version
 }
 
-# If version mismatch install dotnetcli locally
-if ($FoundDotNetCliVersion -ne $DotNetSdkVersion) {
-    Write-Verbose -Message "Installing .NET Core SDK Binaries..."
+if ($FoundDotNetSdkVersion -ne $DotNetSdkVersion) {
+    Write-Verbose -Message "Installing .NET Core $DotNetSdkVersion SDK..."
+
     $InstallPath = Join-Path $PSScriptRoot ".dotnet"
-    if (!(Test-Path $InstallPath)) {
-        mkdir -Force $InstallPath | Out-Null;
+    if (-not (Test-Path $InstallPath)) {
+        mkdir -Force $InstallPath | Out-Null
     }
-    # The remote server returned an error: (407) Proxy Authentication Required, if behind proxy
-    (New-Object System.Net.WebClient).DownloadFile("https://dot.net/v1/dotnet-install.ps1", "$InstallPath\dotnet-install.ps1");
-    # Install .NET SDK (with the latest 2.x runtime)
-    & $InstallPath\dotnet-install.ps1 -Version $DotNetSdkVersion -InstallDir $InstallPath -NoPath;
-    # Install (another..older) .NET Runtime (1.1.x)
-    if ($DotNetRuntimeVersion) {
-        & $InstallPath\dotnet-install.ps1 -SharedRuntime -Version $DotNetRuntimeVersion -InstallDir $InstallPath -NoPath;
-    }
+
+    (New-Object System.Net.WebClient).DownloadFile("https://dot.net/v1/dotnet-install.ps1", "$InstallPath\dotnet-install.ps1")
+
+    # Install .NET SDK (with the latest 2.1.x runtime)
+    & $InstallPath\dotnet-install.ps1 -Channel $DotNetChannel -Version $DotNetSdkVersion -InstallDir $InstallPath -NoPath
 
     Remove-PathVariable "$InstallPath"
     $env:PATH = "$InstallPath;$env:PATH"
     $env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1 # Caching packages on a temporary build machine is a waste of time.
     $env:DOTNET_CLI_TELEMETRY_OPTOUT=1       # opt out of telemetry
+}
 
-    & "$InstallPath\dotnet.exe" --info
+###########################################################################
+# Install .NET Core Runtimes
+###########################################################################
+
+if ($DotNetRuntimeVersions) {
+
+    # Get list of available (global) runtimes installed (runtimes are independent of SDKs)
+    $dotnetGlobalRuntimes = dotnet --list-runtimes
+
+    $InstallPath = Join-Path $PSScriptRoot ".dotnet"
+
+    if (-not (Test-Path "$InstallPath\dotnet-install.ps1")) {
+        if (-not (Test-Path $InstallPath)) {
+            mkdir -Force $InstallPath | Out-Null
+        }
+        (New-Object System.Net.WebClient).DownloadFile("https://dot.net/v1/dotnet-install.ps1", "$InstallPath\dotnet-install.ps1")
+    }
+
+    foreach ($runtime in $DotNetRuntimeVersions.Split(';')) {
+        Write-Verbose "Should we install .NET Core $runtime Runtime..."
+        if (-not ($dotnetGlobalRuntimes | Where-Object {$_ -like "*$runtime*"} )) {
+            $dotnetRuntimePath = Join-Path -Path $InstallPath -ChildPath "shared\Microsoft.NETCore.App" | Join-Path -ChildPath $runtime
+            if (-not (Test-Path $dotnetRuntimePath -PathType Container)) {
+                Write-Verbose -Message "Installing .NET Core $runtime Runtime..."
+                try {
+                    # Install .NET Core Runtime (not the entire SDK) -- Channel has no effect when -Version is given.
+                    & $InstallPath\dotnet-install.ps1 -Runtime dotnet -Version $runtime -InstallDir $InstallPath -NoPath -ErrorAction Continue
+                    Write-Verbose ('Successfully downloaded .NET Core {0}.' -f $runtime)
+                }
+                catch {
+                    Write-Error -ErrorRecord $_
+                }
+            }
+            else {
+                Write-Verbose -Message ".NET Core Runtime version $runtime is already locally installed."
+            }
+        }
+        else {
+            Write-Verbose -Message ".NET Core Runtime version $runtime is already globally installed."
+        }
+    }
 }
 
 $env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
@@ -162,8 +205,8 @@ function MD5HashFile([string] $filePath)
         return $null
     }
 
-    [System.IO.Stream] $file = $null;
-    [System.Security.Cryptography.MD5] $md5 = $null;
+    [System.IO.Stream] $file = $null
+    [System.Security.Cryptography.MD5] $md5 = $null
     try
     {
         $md5 = [System.Security.Cryptography.MD5]::Create()
@@ -172,7 +215,7 @@ function MD5HashFile([string] $filePath)
     }
     finally
     {
-        if ($file -ne $null)
+        if ($null -ne $file)
         {
             $file.Dispose()
         }
@@ -249,7 +292,7 @@ else {
         configuration=$Configuration;
         verbosity=$Verbosity;
         dryrun=$WhatIf;
-    }.GetEnumerator() | ForEach-Object {"--{0}=`"{1}`"" -f $_.key, $_.value };
+    }.GetEnumerator() | ForEach-Object {"--{0}=`"{1}`"" -f $_.key, $_.value }
 
     Write-Host "Running build script..."
     Invoke-Expression "& `"$CAKE_EXE`" `"build.cake`" $Arguments $ScriptArgs"
---
+++ src/Maxfire.Prelude.Core/Linq/EnumerableExtensions.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Maxfire.Prelude.Linq
+{
+    public static class EnumerableExtensions
+    {
+        [DebuggerStepThrough]
+        public static IEnumerable<TResult> Map<T, TResult>(this IEnumerable<T> values, Func<T, TResult> projection)
+        {
+            foreach (T item in values)
+            {
+                yield return projection(item);
+            }
+        }
+
+        [DebuggerStepThrough]
+        public static IEnumerable<TResult> Map<T, TResult>(this IEnumerable<T> values, Func<T, int, TResult> projectionWithIndex)
+        {
+            int index = 0;
+            foreach (T item in values)
+            {
+                yield return projectionWithIndex(item, index);
+                index++;
+            }
+        }
+
+        [DebuggerStepThrough]
+        [SuppressMessage("ReSharper", "PossibleMultipleEnumeration")]
+        public static IEnumerable Each(this IEnumerable values, Action<object> eachAction)
+        {
+            foreach (var item in values)
+            {
+                eachAction(item);
+            }
+
+            return values;
+        }
+
+        [DebuggerStepThrough]
+        [SuppressMessage("ReSharper", "PossibleMultipleEnumeration")]
+        public static IEnumerable Each(this IEnumerable values, Action<object, int> eachAction)
+        {
+            int i = 0;
+            foreach (var item in values)
+            {
+                eachAction(item, i);
+                i++;
+            }
+
+            return values;
+        }
+
+        [DebuggerStepThrough]
+        [SuppressMessage("ReSharper", "PossibleMultipleEnumeration")]
+        public static IEnumerable<T> Each<T>(this IEnumerable<T> values, Action<T> eachAction)
+        {
+            foreach (var item in values)
+            {
+                eachAction(item);
+            }
+
+            return values;
+        }
+
+        [DebuggerStepThrough]
+        [SuppressMessage("ReSharper", "PossibleMultipleEnumeration")]
+        public static IEnumerable<T> Each<T>(this IEnumerable<T> values, Action<T, int> eachAction)
+        {
+            int i = 0;
+            foreach (var item in values)
+            {
+                eachAction(item, i);
+                i++;
+            }
+
+            return values;
+        }
+    }
+}
